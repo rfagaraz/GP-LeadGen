4c7467bd3bd98eaff63b69f7eb8f09902ed0081d
Renato Fagaraz <rfagaraz@gmail.com>
2019-10-10 12:37:07 -0300

    Todas as funções exceto .drop funcionando

--- Lead Generation - Database_func_breakdown.py
+++ Lead Generation - Database_func_breakdown.py
@@ -32,29 +32,29 @@ writer = pd.ExcelWriter(templateBook, engine='openpyxl') #This path leads to the
 writer.book = book
 writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
 
-def regex(all_data):
+def regex(x):
        print('iniciando correções')
-       all_data = all_data.replace(to_replace ="^(\+55|55|055|\s\+55|\s55|\s055){0,1}?(\s|-|\.|\+|\_)?(0{0,1}\s{0,1})?([1-9][0-9]|\([1-9][0-9]\))(\s|-|\.|\+|\_){0,2}?((?!9999)\d{4,5}){0,1}?(\s|-|\.|\+|\_)?((?!0000)\d{4,5})$", value=r"\4-\6\8", regex=True, inplace=True)
-       all_data = all_data.filter(regex="^(\+55|55|055|\s\+55|\s55|\s055){0,1}?(\s|-|\.|\+|\_)?(0{0,1}\s{0,1})?([1-9][0-9]|\([1-9][0-9]\))(\s|-|\.|\+|\_){0,2}?(\d{4,5}){0,1}?(\s|-|\.|\+|\_)?((?!0000)\d{4,5})$", axis=1)
+       x.replace(to_replace ="^(\+55|55|055|\s\+55|\s55|\s055){0,1}?(\s|-|\.|\+|\_)?(0{0,1}\s{0,1})?([1-9][0-9]|\([1-9][0-9]\))(\s|-|\.|\+|\_){0,2}?((?!9999)\d{4,5}){0,1}?(\s|-|\.|\+|\_)?((?!0000)\d{4,5})$", value=r"\4-\6\8", regex=True, inplace=True)
+       x.filter(regex="^(\+55|55|055|\s\+55|\s55|\s055){0,1}?(\s|-|\.|\+|\_)?(0{0,1}\s{0,1})?([1-9][0-9]|\([1-9][0-9]\))(\s|-|\.|\+|\_){0,2}?(\d{4,5}){0,1}?(\s|-|\.|\+|\_)?((?!0000)\d{4,5})$", axis=1)
        print('correções finalizadas!')
-       return all_data
+       return x
 
-def dropRegex(all_data):
-       print("processando")
-       all_data = all_data.drop(all_data[all_data['Phone Mask'].map(len) < 11].index)
+def dropRegex(x):
+       print("droppando")
+       x = x.drop(x[x['Phone Mask'].map(len) < 11].index)
        print("finalizado")
-       return all_data
+       return x
 
 
-def saveFile (all_data):
+def saveFile (x):
        print('Salvando...')
-       all_data = all_data.to_excel(writer, sheet_name='Base Original') #Excel folder which it'll be pasted on
+       x = x.to_excel(writer, sheet_name='Base Original') #Excel folder which it'll be pasted on
        writer.save()
-       return all_data
+       return x
        print('Finalizado')
 
 
 
-regex(all_data)
-dropRegex(all_data)
-saveFile(all_data)
+
+#dropRegex(regex(all_data))
+saveFile(dropRegex(regex(all_data)))
